6f90b3a2321a65592ad011397942d6ff40db2624
Elizabeth Alexander <eealexander2@gmail.com>
2016-06-30 10:49:59 -0700

    Add fixed leads ajax

--- DevBright/app/controllers/homes_controller.rb
+++ DevBright/app/controllers/homes_controller.rb
@@ -1,7 +1,20 @@
 class HomesController < ApplicationController
-	
-	def index 
-		@homes = Home.all
+
+	def index 		
+		if request.xhr? 
+			@homes = Home.where(title: params['home']['title'])
+			if @homes.length > 0 
+			  @homes = Home.where(title: params['home']['title'])  	
+			  render '_find_home', layout: false 
+			else 
+				@homes = Home.all 
+				@message = "Sorry, we could not find the result you were looking for. Please search again!"
+	  		render '_find_home', layout: false 
+	  	end
+	  else 
+			@homes = Home.all 
+	  end 	
+
 	end 
 		
 end
